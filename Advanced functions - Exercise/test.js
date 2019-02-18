148417899410f4ebaadc05f01d189cab7ace1609
superfan <superfn@protonmail.com>
2019-02-18 21:05:20 +0200

    removed test & edited test.js

--- Advanced functions - Exercise/test.js
+++ Advanced functions - Exercise/test.js
@@ -1,13 +1,34 @@
-function Product(name, price) {
-    this.name = name;
-    this.price = price;
+// function Product(name, price) {
+//     this.name = name;
+//     this.price = price;
+//   }
+  
+//   function Food(name, price) {
+//     Product.call(this, name, price);
+//     this.category = 'food';
+//   }
+  
+//   //console.log(new Food('cheese', 5).name);
+//   // expected output: "cheese"
+  
+
+//   let sampleObj = {
+//     'a': 123,
+//     'b': 'number',
+//     'c': 13
+//   };
+
+//   [['a', 123], ['b', 'number']];
+
+// sampleObj = Object.entries(sampleObj).sort((a, b) => b[0].localeCompare(a[0]));
+// console.log(sampleObj);
+
+function revStr(input) {
+  let output = '';
+  for (let c of input) {
+    output = c + output;
   }
-  
-  function Food(name, price) {
-    Product.call(this, name, price);
-    this.category = 'food';
-  }
-  
-  console.log(new Food('cheese', 5).name);
-  // expected output: "cheese"
-  
+  console.log(output)
+}
+
+revStr('test')
